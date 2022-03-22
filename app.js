e41e72b8127a133f382f55ebfb372404cc4ba73b
HoriCap997 <horicap997@gmail.com>
2022-03-22 11:47:09 +0800

    Added function to change grid size

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-let container = document.getElementById("container");
 
+const grid = document.getElementById("grid");
 
-function makeGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+const defaultSize = 16;
+let currentSize = defaultSize; 
+
+function makeGrid(currentSize) {
+    grid.style.setProperty('--gridRows', currentSize);
+    grid.style.setProperty('--gridCols', currentSize);
+    for (c = 0; c < (currentSize * currentSize); c++) {
         let cell = document.createElement("div"); 
-        container.appendChild(cell).className = "gridItem";
+        grid.appendChild(cell).className = "gridItem";
+        cell.addEventListener("mouseover",changeColor);
+        cell.addEventListener("mousedown",changeColor);
     };
   }; 
 
-makeGrid(16,16)
-const gridItems= Array(document.querySelectorAll(".gridItem"));
-console.log(gridItems)
+makeGrid(currentSize)
+
+const defaultColor = '#333333';
+
+let currentColor = defaultColor;
+let mouseDown = false;  
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeColor(e){
+    if (e.type === "mouseover" && !mouseDown) return; 
+    e.target.style.backgroundColor = "currentColor";
+}
+
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+
+sizeSlider.onmousemove = (e) => updateSizeText(e.target.value);
+sizeSlider.onchange = (e) => updateGrid(e.target.value);
+
+function updateGrid(newSize){
+    updateSizeText(newSize);
+    setCurrentSize(newSize);
+    reloadGrid(newSize)
+}
+
+function setCurrentSize(newSize){
+    let currentSize = newSize;
+}
+function updateSizeText(newSize){
+    sizeValue.innerText= `${newSize} x ${newSize}`
+}
+
+
+function reloadGrid(newSize){
+    grid.innerHTML=('') 
+    makeGrid(newSize);
+}
